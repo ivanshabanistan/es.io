a50263985b7a83f8587b364848865e983effdd64
Ihar Skavysh <igogoblin@gmail.com>
2024-10-21 17:33:03 +0300

    feat: add basename

--- es/src/App.jsx
+++ es/src/App.jsx
@@ -8,7 +8,7 @@ import Contacts from "./pages/Contacts";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/es/">
       <Layout />
       <Routes>
         <Route index element={<Home />} />
